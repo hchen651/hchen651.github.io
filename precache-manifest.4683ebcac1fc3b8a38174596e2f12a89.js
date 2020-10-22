28c140f012598c7357aa50a3d4e4962290508788
Henry <hchen651@gmail.com>
2020-10-22 16:22:13 -0400

    Updates

---
+++ precache-manifest.4683ebcac1fc3b8a38174596e2f12a89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2f76270b55c740f5b92ad217094f92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbf4c2ecb257bc942350",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c0c92013958636ecef52",
+    "url": "/static/js/2.63eb4406.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.63eb4406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf4c2ecb257bc942350",
+    "url": "/static/js/main.c38c7987.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
